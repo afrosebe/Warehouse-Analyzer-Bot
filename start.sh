481399a67b74d67847d73b3bf128fc510fdb30e7
Afrose <102590142+afrosebe@users.noreply.github.com>
2026-06-12 23:34:08 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Start Warehouse Auction Analyzer Bot
-cd /data/data/com.termux/files/home/warehouse-analyzer
-export BOT_TOKEN="8932041732:AAE3qq-jv3t9FpWngCZHTAn-Jgzbmcb_nn0"
-nohup python3 bot.py > bot.log 2>&1 &
-echo "Bot started! PID: $!"
-echo "Check status: tail -f bot.log"
